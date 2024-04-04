374af4748d48af47147610f6d1fa3901040fbf47
islam302 <islambadran39@gmail.com>
2024-04-04 08:00:41 +0200

    Add Seach about your new in Google

--- dd.py
+++ dd.py
@@ -1,41 +1,267 @@
+# # import re
+# # from bs4 import BeautifulSoup
+# # import requests
+# # import pandas as pd
+# # import difflib
+# # import pycountry
+# # import chardet
+# # from urllib.parse import quote
+# #
+# #
+# # class Search_About_News:
+# #
+# #     def get_links_from_file(self, file_path):
+# #         with open(file_path, 'r', encoding='utf-8') as file:
+# #             links = file.readlines()
+# #         return [link.strip() for link in links if link.strip()]
+# #
+# #     def get_words_from_file(self, words_file_path):
+# #         with open(words_file_path, 'r', encoding='utf-8') as file:
+# #             words = file.readlines()
+# #         return list(set([word.strip() for word in words if word.strip()]))
+# #
+# #     def get_response(self, words, links):
+# #         found_links = {}
+# #         for word in words:
+# #             found_links[word] = []
+# #             for link in links:
+# #                 url = f'{link}{word}'
+# #                 response = requests.get(url)
+# #                 if response.status_code == 200:
+# #                     soup = BeautifulSoup(response.text, 'html.parser')
+# #                     target_pattern = re.compile(word)
+# #                     for line in soup.stripped_strings:
+# #                         if target_pattern.search(line):
+# #                             parent_a = soup.find('a', string=line)
+# #                             if parent_a and 'href' in parent_a.attrs:
+# #                                 found_links[word].append(parent_a['href'])
+# #                                 break
+# #                 else:
+# #                     with open('links_need_to_done_manually.txt', 'a', encoding='utf-8') as file:
+# #                         file.write(f'{link}\n')
+# #         return found_links
+# #
+# #     def get_countries_from_file(self, countries_file_path):
+# #         with open(countries_file_path, 'r', encoding='utf-8') as file:
+# #             return [country.strip() for country in file if country.strip()]
+# #
+# #     def create_google_search_links(self, countries):
+# #         google_links = []
+# #         for country in countries:
+# #             try:
+# #                 country_obj = pycountry.countries.lookup(country)
+# #                 zip_code = country_obj.alpha_2.lower()
+# #                 google_links.append(f"https://www.google.com.{zip_code}/")
+# #             except LookupError:
+# #                 print(f"Country '{country}' not found. Skipping...")
+# #         return google_links
+# #
+# #     def search_and_get_links(self, query, num_results=10):
+# #         try:
+# #             search_results = search(query, num_results=num_results, lang='en')
+# #             return list(search_results)
+# #         except Exception as e:
+# #             print(f"An error occurred: {e}")
+# #             return []
+# #
+# #     def search_for_word_in_google(self, words, arab_countries_google_urls):
+# #         result_links = {}
+# #         for word in words:
+# #             result_links[word] = []
+# #             for url in arab_countries_google_urls:
+# #                 search_url = f'{url}search?q={word}'
+# #                 try:
+# #                     response = requests.get(search_url)
+# #                     if response.status_code == 200:
+# #                         soup = BeautifulSoup(response.content, "html.parser")
+# #                         links = soup.find_all("a")
+# #                         links_found = 0
+# #                         for link in links:
+# #                             href = link.get("href")
+# #                             if href.startswith("/url?q="):
+# #                                 result_links[word].append(href.replace("/url?q=", "").split("&sa=")[0])
+# #                                 links_found += 1
+# #                                 if links_found >= 10:
+# #                                     break
+# #                 except requests.exceptions.RequestException as e:
+# #                     continue
+# #
+# #         return result_links
+# #
+# #     def main(self):
+# #
+# #         words = self.get_words_from_file('news.txt')
+# #         x = input('Start Task From Domains enter 1\nFrom Google enter 2\n')
+# #
+# #         if x == '1':
+# #
+# #             links = self.get_links_from_file('links.txt')
+# #             found_links = self.get_response(words, links)
+# #             df = pd.DataFrame.from_dict(found_links, orient='index').transpose()
+# #             for col in df.columns:
+# #                 max_len = max([len(str(cell)) for cell in df[col]])
+# #                 width = max(50, max_len)
+# #                 df[col] = df[col].astype(str).str.pad(width, side='right')
+# #             df.to_excel('found_links_From_Domains.xlsx', index=False)
+# #             print(f'The links were found and saved in "found_links.xlsx".')
+# #
+# #         else:
+# #             countries = self.get_countries_from_file('countries.txt')
+# #             searching_urls = self.create_google_search_links(countries)
+# #             found = self.search_for_word_in_google(words, searching_urls)
+# #             df = pd.DataFrame.from_dict(found, orient='index').transpose()
+# #             if not df.empty:
+# #                 max_len = max([len(str(cell)) for col in df.columns for cell in df[col]])
+# #                 for col in df.columns:
+# #                     max_len = max([len(str(cell)) for cell in df[col]])
+# #                     width = max(50, max_len)
+# #                     df[col] = df[col].astype(str).str.pad(width, side='right')
+# #                 df.to_excel('found_links_From_Google.xlsx', index=False)
+# #                 print(f'The links were found and saved in "found_links.xlsx".')
+# #             else:
+# #                 print("No results found. No links were saved.")
+# #
+# #
+# # if __name__ == '__main__':
+# #     bot = Search_About_News()
+# #     bot.main()
+# #
+#
+#
+
+
 import re
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+import difflib
+import pycountry
+import chardet
+from urllib.parse import quote
+import os
 
 class Search_About_News:
 
     def get_links_from_file(self, file_path):
         with open(file_path, 'r', encoding='utf-8') as file:
             links = file.readlines()
-        return [link.strip() for link in links]
+        return [link.strip() for link in links if link.strip()]
+
+    def get_words_from_file(self, words_file_path):
+        with open(words_file_path, 'r', encoding='utf-8') as file:
+            words = file.readlines()
+        return list(set([word.strip() for word in words if word.strip()]))
 
     def get_response(self, words, links):
-        found_links = []
+        found_links = {}
         for word in words:
+            found_links[word] = []
             for link in links:
-                url = f'{link}search?q={word}'
+                url = f'{link}{word}'
                 response = requests.get(url)
                 if response.status_code == 200:
                     soup = BeautifulSoup(response.text, 'html.parser')
-                    # تحديد النص بواسطة تعبير منتظم
-                    target_pattern = re.compile(r'د\.العيسى يُعلن الإطلاق "التجريبي" للمتحف الدولي للسيرة النبويّة بأبراج الساعة')
-                    for line in soup.stripped_strings:
-                        if target_pattern.search(line):
-                            parent_a = soup.find('a', string=line)
-                            if parent_a and 'href' in parent_a.attrs:
-                                found_links.append(parent_a['href'])
-                                break
-
+                    for a_tag in soup.find_all('a', href=True):
+                        if word in a_tag.get_text():
+                            found_links[word].append(a_tag['href'])
+                            break
                 else:
-                    print(f"Failed to fetch {link}")
+                    with open('links_need_to_done_manually.txt', 'a', encoding='utf-8') as file:
+                        file.write(f'{link}\n')
         return found_links
 
+    def get_countries_from_file(self, countries_file_path):
+        with open(countries_file_path, 'r', encoding='utf-8') as file:
+            return [country.strip() for country in file if country.strip()]
+
+    def create_google_search_links(self, countries):
+        google_links = []
+        for country in countries:
+            try:
+                country_obj = pycountry.countries.lookup(country)
+                zip_code = country_obj.alpha_2.lower()
+                google_links.append(f"https://www.google.com.{zip_code}/")
+            except LookupError:
+                print(f"Country '{country}' not found. Skipping...")
+        return google_links
+
+    def search_and_get_links(self, query, num_results=10):
+        try:
+            search_results = search(query, num_results=num_results, lang='en')
+            return list(search_results)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return []
+
+    def search_for_word_in_google(self, words, arab_countries_google_urls):
+        result_links = {}
+        for word in words:
+            result_links[word] = []
+            for url in arab_countries_google_urls:
+                search_url = f'{url}search?q={word}'
+                try:
+                    response = requests.get(search_url)
+                    if response.status_code == 200:
+                        soup = BeautifulSoup(response.content, "html.parser")
+                        links = soup.find_all("a")
+                        links_found = 0
+                        for link in links:
+                            href = link.get("href")
+                            if href.startswith("/url?q="):
+                                result_links[word].append(href.replace("/url?q=", "").split("&sa=")[0])
+                                links_found += 1
+                                if links_found >= 10:
+                                    break
+                except requests.exceptions.RequestException as e:
+                    continue
+
+        return result_links
+
+
     def main(self):
-        links = self.get_links_from_file('links.txt')
-        found_links = self.get_response(words=[], links=links)
-        print(f'The links were found in the following links:')
-        print(found_links)
+        x = input('Start Task From Domains enter 1\nFrom Google enter 2\n')
+        words = self.get_words_from_file('news.txt')
+        if x == '1':
+            links = self.get_links_from_file('links.txt')
+            found_links = self.get_response(words, links)
+            print(found_links)
+
+            df = pd.DataFrame.from_dict(found_links, orient='index').transpose()
+            df = df.fillna('')  # Replace any remaining None values with empty string
+            for col in df.columns:
+                max_len = max([len(str(cell)) for cell in df[col]])
+                width = max(50, max_len)
+                df[col] = df[col].astype(str).str.pad(width, side='right')
+
+            if os.path.exists('found_links_From_Domains.xlsx'):
+                os.remove('found_links_From_Domains.xlsx')
+
+            df.to_excel('found_links_From_Domains.xlsx', index=False)  # Save new data
+            print(f'The links were found and saved in "found_links_From_Domains.xlsx".')
+
+        else:
+            countries = self.get_countries_from_file('countries.txt')
+            searching_urls = self.create_google_search_links(countries)
+            found = self.search_for_word_in_google(words, searching_urls)
+            df = pd.DataFrame.from_dict(found, orient='index').transpose()
+            df = df.fillna('')
+            if not df.empty:
+                max_len = max([len(str(cell)) for col in df.columns for cell in df[col]])
+                for col in df.columns:
+                    max_len = max([len(str(cell)) for cell in df[col]])
+                    width = max(50, max_len)
+                    df[col] = df[col].astype(str).str.pad(width, side='right')
+
+                if os.path.exists('found_links_From_Google.xlsx'):
+                    os.remove('found_links_From_Google.xlsx')
+
+                df.to_excel('found_links_From_Google.xlsx', index=False)
+                print(f'The links were found and saved in "found_links_From_Google.xlsx".')
+            else:
+                print("No results found. No links were saved.")
+
 
 if __name__ == '__main__':
     bot = Search_About_News()
     bot.main()
+
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import chardet
 import re
-import requests
-import pycountry
 from bs4 import BeautifulSoup
-from urllib.parse import quote
-from urllib.parse import urlparse
+import requests
+import pandas as pd
+import difflib
+import pycountry
+import chardet
 
 
 class Search_About_News:
@@ -12,192 +12,118 @@ class Search_About_News:
     def get_links_from_file(self, file_path):
         with open(file_path, 'r', encoding='utf-8') as file:
             links = file.readlines()
-        return [link.strip() for link in links]
+        return [link.strip() for link in links if link.strip()]
 
     def get_words_from_file(self, words_file_path):
         with open(words_file_path, 'r', encoding='utf-8') as file:
             words = file.readlines()
-        return [word.strip() for word in words]
+        return list(set([word.strip() for word in words if word.strip()]))
 
     def get_response(self, words, links):
-        found_in_links = []
+        found_links = {}
         for word in words:
+            found_links[word] = []
             for link in links:
-                if link.startswith('https://'):
-                    url = f'{link}search?q={word}'
-                else:
-                    url = f'https://{link}search?q={word}'
                 response = requests.get(url)
                 if response.status_code == 200:
+                    print(link)
                     soup = BeautifulSoup(response.text, 'html.parser')
-                    target_pattern = re.compile(
-                        r'د\.العيسى يُعلن الإطلاق “التجريبي” للمتحف الدولي للسيرة النبويّة بأبراج الساعة')
-                    for a_tag in soup.find_all('a', href=True):
-                        if target_pattern.search(a_tag.get_text()):
-                            found_in_links.append(a_tag['href'])
-                            break
+                    target_pattern = re.compile(word)
+                    for line in soup.stripped_strings:
+                        if target_pattern.search(line):
+                            parent_a = soup.find('a', string=line)
+                            if parent_a and 'href' in parent_a.attrs:
+                                found_links[word].append(parent_a['href'])
+                                break
+                    break
                 else:
-                    print(f"Failed to fetch {link}")
-        return found_in_links
+                    with open('links_need_to_done_manually.txt', 'a', encoding='utf-8') as file:
+                        file.write(f'{link}\n')
+        return found_links
+
+    def get_countries_from_file(self, countries_file_path):
+        with open(countries_file_path, 'r', encoding='utf-8') as file:
+            return [country.strip() for country in file if country.strip()]
+
+    def create_google_search_links(self, countries):
+        google_links = []
+        for country in countries:
+            try:
+                country_obj = pycountry.countries.lookup(country)
+                zip_code = country_obj.alpha_2.lower()
+                google_links.append(f"https://www.google.com.{zip_code}/")
+            except LookupError:
+                print(f"Country '{country}' not found. Skipping...")
+        return google_links
+
+    def search_and_get_links(self, query, num_results=10):
+        try:
+            search_results = search(query, num_results=num_results, lang='en')
+            return list(search_results)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return []
+
+    def search_for_word_in_google(self, words, arab_countries_google_urls):
+        result_links = {}
+        for word in words:
+            result_links[word] = []
+            links_found = 0
+            for url in arab_countries_google_urls:
+                search_url = f'{url}search?q={word}'
+                try:
+                    response = requests.get(search_url)
+                    if response.status_code == 200:
+                        soup = BeautifulSoup(response.content, "html.parser")
+                        links = soup.find_all("a")
+                        for link in links:
+                            href = link.get("href")
+                            if href.startswith("/url?q="):
+                                result_links[word].append(href.replace("/url?q=", "").split("&sa=")[0])
+                                links_found += 1
+                                if links_found >= 10:
+                                    break
+                except requests.exceptions.RequestException as e:
+                    print(f"An error occurred: {e}")
+                    continue
+
+        return result_links
+
+
 
     def main(self):
-        links = self.get_links_from_file('links.txt')
         words = self.get_words_from_file('news.txt')
-        found_in_links = self.get_response(words, links)
-        print(f'The words were found in the following links:')
-        print(found_in_links)
+        x = input('Start Task From Domains enter 1\nFrom Google enter 2\n')
+        if x == '1':
 
+            links = self.get_links_from_file('links.txt')
+            found_links = self.get_response(words, links)
+            df = pd.DataFrame.from_dict(found_links, orient='index').transpose()
+            for col in df.columns:
+                max_len = max([len(str(cell)) for cell in df[col]])
+                width = max(50, max_len)
+                df[col] = df[col].astype(str).str.pad(width, side='right')
+            df.to_excel('found_links_From_Domains.xlsx', index=False)
+            print(f'The links were found and saved in "found_links.xlsx".')
+
+        else:
+            countries = self.get_countries_from_file('countries.txt')
+            searching_urls = self.create_google_search_links(countries)
+            found = self.search_for_word_in_google(words, searching_urls)
+            df = pd.DataFrame.from_dict(found, orient='index').transpose()
+            if not df.empty:
+                max_len = max([len(str(cell)) for col in df.columns for cell in df[col]])
+                for col in df.columns:
+                    max_len = max([len(str(cell)) for cell in df[col]])
+                    width = max(50, max_len)
+                    df[col] = df[col].astype(str).str.pad(width, side='right')
+                df.to_excel('found_links_From_Google.xlsx', index=False)
+                print(f'The links were found and saved in "found_links.xlsx".')
+            else:
+                print("No results found. No links were saved.")
 
 
 if __name__ == '__main__':
     bot = Search_About_News()
     bot.main()
-
-
-# #
-# #     #
-# #     #
-# #     #
-# #     #
-# #     # # def search_for_word_in_google(self, word, arab_countries_google_urls):
-# #     # #     for url in arab_countries_google_urls:
-# #     # #         search_url = f'{url}search?q={word}'
-# #     # #         try:
-# #     # #             response = requests.get(search_url)
-# #     # #             if response.status_code == 200:
-# #     # #                 encoding = chardet.detect(response.content)['encoding']
-# #     # #                 soup = BeautifulSoup(response.content.decode(encoding), 'html.parser')
-# #     # #                 results = soup.find_all('a', href=True)
-# #     # #                 for result in results:
-# #     # #                     if 'url?q=' in result['href']:
-# #     # #                         link = result['href'].split('url?q=')[1].split('&sa=')[0]
-# #     # #                         if word in result.get_text():
-# #     # #                             print(f'The word "{word}" was found in the following link:')
-# #     # #                             print(link)
-# #     # #                             break
-# #     # #             else:
-# #     # #                 print(f'Failed to fetch search results from {url}')
-# #     # #         except:
-# #     # #             print(f"Failed to fetch search results from {search_url}. Skipping...")
-# #     # #             continue
-# #     #
-# #     # def main(self):
-# #     #
-# #     #     def get_countries_from_file(countries_file_path):
-# #     #         with open(countries_file_path, 'r') as file:
-# #     #             countries = file.readlines()
-# #     #         return [country.strip() for country in countries]
-# #     #
-# #     #     def get_words_from_file(words_file_path):
-# #     #         with open(words_file_path, 'r') as file:
-# #     #             words = file.readlines()
-# #     #         return [word.strip() for word in words]
-# #     #
-# #     #     def create_google_search_links(countries):
-# #     #         google_links = []
-# #     #         for country in countries:
-# #     #             try:
-# #     #                 country_obj = pycountry.countries.lookup(country)
-# #     #                 zip_code = country_obj.alpha_2
-# #     #                 google_links.append(f"https://www.google.com.{zip_code}/")
-# #     #             except LookupError:
-# #     #                 print(f"Country '{country}' not found. Skipping...")
-# #     #                 continue
-# #     #         return google_links
-# #     #
-# #     #     countries_file_path = 'countries.txt'
-# #     #     words_file_path = 'news.txt'
-# #     #     countries = get_countries_from_file(countries_file_path)
-# #     #     words_to_check = get_words_from_file(words_file_path)
-# #     #     # arab_countries_google_urls = create_google_search_links(countries)
-# #     #
-# #     #     self.list_of_links = self.get_links_from_file('links.txt')
-# #     #     responses = self.get_response(self.list_of_links)
-# #     #
-# #     #
-# #     #     # self.search_for_word_in_google('د.العيسى يُعلن الإطلاق “التجريبي” للمتحف الدولي للسيرة النبويّة بأبراج الساعة', arab_countries_google_urls)
-#
-#
-# import requests
-# from bs4 import BeautifulSoup
-#
-# class Search_About_News:
-#
-#     def get_links_from_file(self, file_path):
-#         with open(file_path, 'r', encoding='utf-8') as file:
-#             links = file.readlines()
-#         return [link.strip() for link in links]
-#
-#     def get_response(self, links):
-#         found_links = []
-#         for link in links:
-#             response = requests.get(link)
-#             if response.status_code == 200:
-#                 soup = BeautifulSoup(response.text, 'html.parser')
-#                 # تقسيم النص إلى أسطر
-#                 lines = soup.get_text().splitlines()
-#                 target_text = 'د.العيسى يُعلن الإطلاق “التجريبي” للمتحف الدولي للسيرة النبويّة بأبراج الساعة'
-#                 for line in lines:
-#                     if target_text in line:
-#                         parent_a = soup.find('a', string=line)
-#                         if parent_a and 'href' in parent_a.attrs:
-#                             found_links.append(parent_a['href'])
-#                             break
-#             else:
-#                 print(f"Failed to fetch {link}")
-#         return found_links
-#
-#     def main(self):
-#         links = self.get_links_from_file('links.txt')
-#         found_links = self.get_response(links)
-#
-# if __name__ == '__main__':
-#     bot = Search_About_News()
-#     bot.main()
-#
-
-
-
-
-# import re
-# from bs4 import BeautifulSoup
-# import requests
-#
-# class Search_About_News:
-#
-#     def get_links_from_file(self, file_path):
-#         with open(file_path, 'r', encoding='utf-8') as file:
-#             links = file.readlines()
-#         return [link.strip() for link in links]
-#
-    # def get_response(self, words, links):
-    #     found_links = []
-    #     for link in links:
-    #         response = requests.get(link)
-    #         if response.status_code == 200:
-    #             soup = BeautifulSoup(response.text, 'html.parser')
-    #             # تحديد النص بواسطة تعبير منتظم
-    #             target_pattern = re.compile(r'د\.العيسى يُعلن الإطلاق "التجريبي" للمتحف الدولي للسيرة النبويّة بأبراج الساعة')
-    #             for line in soup.stripped_strings:
-    #                 if target_pattern.search(line):
-    #                     parent_a = soup.find('a', string=line)
-    #                     if parent_a and 'href' in parent_a.attrs:
-    #                         found_links.append(parent_a['href'])
-    #                         break
-    #
-    #         else:
-    #             print(f"Failed to fetch {link}")
-    #     return found_links
-#
-#     def main(self):
-#         links = self.get_links_from_file('links.txt')
-#         found_links = self.get_response(words=[], links=links)
-#         print(f'The links were found in the following links:')
-#         print(found_links)
-#
-# if __name__ == '__main__':
-#     bot = Search_About_News()
-#     bot.main()
-
-
 
